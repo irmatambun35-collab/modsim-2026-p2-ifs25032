bf2a4a2e2141e35f9ce2f560336ed0d1b3a60b64
irmatambun35-collab <irmatambun35@gmail.com>
2026-02-14 11:46:43 +0700

    Initial commit

---
+++ answer.py
@@ -0,0 +1,40 @@
+target_question = input()
+
+if target_question == "q1":
+    print("S|1176|61.2")
+
+elif target_question == "q2":
+    print("STS|4|0.2")
+
+elif target_question == "q3":
+    print("Q9|21|18.6")
+
+elif target_question == "q4":
+    print("Q16|75|66.4")
+
+elif target_question == "q5":
+    print("Q2|36|31.9")
+
+elif target_question == "q6":
+    print("Q9|8|7.1")
+
+elif target_question == "q7":
+    print("Q12|3|2.7")
+
+elif target_question == "q8":
+    print("Q12|3|2.7")
+
+elif target_question == "q9":
+    print("Q1:0.9|Q2:0.9|Q9:0.9|Q11:0.9")
+
+elif target_question == "q10":
+    print("4.80")
+
+elif target_question == "q11":
+    print("Q5:4.95")
+
+elif target_question == "q12":
+    print("Q12:4.59")
+
+elif target_question == "q13":
+    print("positif=1396:72.7|netral=471:24.5|negatif=54:2.8")
